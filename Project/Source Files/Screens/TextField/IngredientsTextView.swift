81bc1fe50d70a3f3f53ec44fba60e2a6b9d8fd80
Aleksander <alexlotr@wp.pl>
2019-08-25 13:01:11 +0200

    Erased some lanes of code, changed basic comment to TODO

--- Project/Source Files/Screens/TextField/IngredientsTextView.swift
+++ Project/Source Files/Screens/TextField/IngredientsTextView.swift
@@ -16,21 +16,6 @@ class IngredientsTextView: UIView {
     @IBOutlet var cameraButtonView: UIButton!
     @IBOutlet var contentView: UIView!
     weak var delegate: IngredientsTextViewDelegate?
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        commonInit()
-    }
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        commonInit()
-    }
-    private func commonInit() {
-        UITextField.init().delegate = self
-        Bundle.main.loadNibNamed("TextField", owner: self, options: nil)
-        addSubview(contentView)
-        contentView.frame = self.bounds
-        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-    }
     override func awakeFromNib() {
         super.awakeFromNib()
         ingredientsTextField.delegate = self
@@ -38,13 +23,13 @@ class IngredientsTextView: UIView {
     class func instanceFromNib() -> UIView {
         return UINib(nibName: "IngredientsTextView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! UIView
     }
+    // TODO: camera button action 
     @IBAction func cameraButtonPressed(_ sender: Any) {
-        /// here ll be camera button action if added
+        
     }
 }
 extension IngredientsTextView: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        /// checking if text isn't nill
         guard let text = textField.text else {
             return true
         }
