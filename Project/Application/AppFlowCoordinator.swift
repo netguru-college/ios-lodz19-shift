7393471e51ab2e02d9cd333e843b82d28080a958
Oskar <riewe.perla@gmail.com>
2019-08-25 10:40:51 +0200

    Finished working on TableViewController and TableViewCell

--- Project/Application/AppFlowCoordinator.swift
+++ Project/Application/AppFlowCoordinator.swift
@@ -24,7 +24,7 @@ final class AppFlowCoordinator: FlowCoordinator {
 extension AppFlowCoordinator: HelloWorldViewControllerDelegate {
 
     func didSelectNextButton() {
-        let nextViewController = WelcomeViewController()
+        let nextViewController = RecipiesTableViewController()
         rootViewController.show(nextViewController, sender: nil)
     }
 }
---
+++ Project/Source Files/Screens/Recipies/RecipiesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipiesTableViewCell.swift
+//  NetguruCollegeApp
+//
+import UIKit
+
+class RecipiesTableViewCell: UITableViewCell {
+
+    @IBOutlet var recipeImage: UIImageView!
+    @IBOutlet var recipeTitle: UILabel!
+    @IBOutlet var recipeIngredients: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
