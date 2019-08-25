3c99ee47b721d046337df0d8ed918a7455b92f0f
Zofia Drabek <zosia.drabek@gmail.com>
2019-08-25 11:56:15 +0200

    Add documentation.

--- Project/Source Files/Services/Networking/Recipes/RecipeRequest.swift
+++ Project/Source Files/Services/Networking/Recipes/RecipeRequest.swift
@@ -5,13 +5,17 @@
 
 import Foundation
 
+/// RecipeRequets has prepared request with ingerdients converted into proper format.
 final class RecipeRequest: APIRequest {
+    
     var path: String = ""
     var method: APIMethod {
         return .get
     }
+    
     var parameters: [String: String]?
     init(with ingredients: [String]) {
         self.parameters = ["i": ingredients.joined(separator: ",")]
     }
+    
 }
