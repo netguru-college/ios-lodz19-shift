50c5964736ec694522b85a93cc7e7c7a8157cd4c
Zofia <zosia.drabek@gmail.com>
2019-08-25 11:28:11 +0200

    Update Project/Application/AppDependencies.swift
    Co-Authored-By: Anna-Mariia Shkarlinska <anna-mariia.shkarlinska@netguru.co>

--- Project/Application/AppDependencies.swift
+++ Project/Application/AppDependencies.swift
@@ -7,5 +7,6 @@ import Foundation
 
 /// The class that keeps dependencies for the app.
 final class AppDependencies {
+
     let appClientForRecipes = APIClient(withURL: "http://www.recipepuppy.com/api/")
 }
